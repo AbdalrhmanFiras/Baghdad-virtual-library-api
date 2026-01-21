281aff47930187441a0bb99307e8beff9100b827
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-21 21:26:37 +0300

    try 6

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // **********************************/Auth/*****************************//
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
+// a
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
