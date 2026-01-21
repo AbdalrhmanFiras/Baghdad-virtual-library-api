7c051a9490248d99186ff259ce63e1f6d6225f20
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-21 23:32:11 +0300

    stop today

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,14 +15,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        // Log all exceptions safely
-        $exceptions->reportable(function (\Throwable $e) {
-            try {
-                if (function_exists('error_log')) {
-                    error_log('Laravel Exception: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine());
-                }
-            } catch (\Throwable $logError) {
-                // Silently fail if logging fails
-            }
-        });
+        //
     })->create();
