e92e5de2f2ca33f0540a4f094c69aac5fad8dec7
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-21 23:14:58 +0300

    try 12

--- config/database.php
+++ config/database.php
@@ -58,8 +58,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //     (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            // ]) : [],
+
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA', null),
+                PDO::MYSQL_ATTR_SSL_MODE => env('DB_SSL_MODE', 'DISABLED'),
             ]) : [],
         ],
 
