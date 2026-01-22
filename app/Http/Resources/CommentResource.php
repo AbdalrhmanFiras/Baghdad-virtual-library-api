97ff47f4bf2c19662b8a6f2a4114e518b91b7acd
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-22 14:46:21 +0300

    fix commentResource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -19,7 +19,7 @@ class CommentResource extends JsonResource
             'id' => $this->id,
             'context' => $this->context,
             'profile_image' => $this->user->profile->image
-                ? Storage::disk('s3')->url($this->user->profile->image->url)
+                ? Storage::disk('s3-private')->url($this->user->profile->image->url)
                 : null,
             'profile_name' => optional($this->user->profile)->fullname,
             'user_id' => $this->user_id,
