a3c6664bf040dcfc8cd8393c0638b1afee6bec56
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-22 14:49:49 +0300

    fix it

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
@@ -19,7 +20,7 @@ class CommentResource extends JsonResource
             'id' => $this->id,
             'context' => $this->context,
             'profile_image' => $this->user->profile->image
-                ? Storage::disk('s3-private')->url($this->user->profile->image->url)
+                ? Storage::disk('s3-private')->temporaryUrl($this->user->profile->image->url,Carbon::now()->addHours(24))
                 : null,
             'profile_name' => optional($this->user->profile)->fullname,
             'user_id' => $this->user_id,
