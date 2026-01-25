a3a6f81d33a65d492b7b5bb467a8b08360b1a601
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-25 11:22:47 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 // **********************************/Auth/*****************************//
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-// Route::get('auth/google', [AuthController::class, 'google']);
-Route::get('auth/google/redirect', [AuthController::class, 'redirect']);
-Route::get('auth/google/callback', [AuthController::class, 'callback']);
+// // Route::get('auth/google', [AuthController::class, 'google']);
+// Route::get('auth/google/redirect', [AuthController::class, 'redirect']);
+// Route::get('auth/google/callback', [AuthController::class, 'callback']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/google/redirect', [AuthController::class, 'redirect']);
+Route::get('auth/google/callback', [AuthController::class, 'callback']);
