004806c718b6d25a81c3f7814adfb14badc1a6ff
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-02-03 21:37:40 +0300

    import the cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,9 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        
+
         // Add CORS middleware globally for API documentation (Scramble) and API routes
-        $middleware->append(\Illuminate\Http\Foundation\Http\Middleware\HandleCors::class);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
